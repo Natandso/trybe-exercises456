305448ce9306dd0d1398f995ecc3755ceb6e1b8c
Natandso <minatosantana@hotmail.com>
2023-02-27 19:54:14 -0300

    Exercicio5

---
+++ 1 Fundamentos/Secao-3-INtroducao-JS/DIA1-JS-PRIMEIROSPASSOS/Exercicio5.js
@@ -0,0 +1,9 @@
+let weekDay = 'Quarta-feira';
+
+if(weekDay === 'Segunda-feira' || weekDay === 'Terça-feira' || weekDay === 'Quarta-feira' || weekDay === 'Quinta-feira' || weekDay === 'Sexta-feira') {
+weekDay = 'Oba, mais um dia de aprendizado na Trybe >:D'
+}else{
+    console.log('FINALMENTE, descanso merecido! UwU!');
+}
+
+console.log(weekDay);
