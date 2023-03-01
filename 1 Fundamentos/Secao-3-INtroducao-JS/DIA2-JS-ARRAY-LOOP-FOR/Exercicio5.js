5f4df49e9d5acd1a1cb5e696913e78e0b66f14c2
Natandso <minatosantana@hotmail.com>
2023-03-01 01:38:38 -0300

    exercicio5AlmostFinished

---
+++ 1 Fundamentos/Secao-3-INtroducao-JS/DIA2-JS-ARRAY-LOOP-FOR/Exercicio5.js
@@ -0,0 +1,40 @@
+let factorial = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+let result = 1;
+
+for (let index = 1; index <= factorial.length; index += 1) {
+    result *= index;
+    console.log(result);
+
+}
+
+let word = 'tryber';
+let changedWord = ''
+
+for (let index = word.length - 1; index >= 0; index -= 1) {
+    changedWord += word[index]
+
+}
+
+console.log(changedWord);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let receiveBig = ''
+let receiveSmall = ''
+
+for (let indexBig = 0; indexBig < array.length; indexBig += 1) {
+    if (indexBig > receiveBig) {
+        receiveBig = array[indexBig]
+
+    }
+}
+console.log(receiveBig);
+
+
+for (let indexSmall = 0; indexSmall < array.length; indexSmall += 1) {
+    if (receiveSmall < indexSmall) {
+        receiveSmall = array[indexSmall]
+}
+}
+console.log(receiveSmall);
+
+
