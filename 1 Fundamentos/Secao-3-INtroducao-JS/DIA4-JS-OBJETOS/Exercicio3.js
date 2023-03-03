41e3700547c524605e8afc4f74c98fe5d1c8636b
Natandso <minatosantana@hotmail.com>
2023-03-02 21:11:56 -0300

    Exercicio3-Objects

---
+++ 1 Fundamentos/Secao-3-INtroducao-JS/DIA4-JS-OBJETOS/Exercicio3.js
@@ -0,0 +1,25 @@
+
+
+let student = {
+    nome: 'Natan',
+    idade: 24
+
+
+};
+
+student.sobrenome = 'Santana'
+let key = 'email';
+email = 'minatoxapana@HTMLOListElement.com'
+
+function addcoisas(object, key, value) {
+    object[key] = value;
+}
+
+
+let keyzada = 'fullName';
+fullName = student.nome + ' ' + student.sobrenome;
+addcoisas(student, keyzada, fullName);
+
+addcoisas(student, key, email,)
+student.gitHub = 'Natandso'
+console.log(student);
